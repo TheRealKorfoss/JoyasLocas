17bf54b91fde1d12adb75321dd187ca4ff40ed34
jil <a20100304@ceti.mx>
2024-10-19 16:39:05 -0600

    esta version solo tiene un header y un par de imagenes de prueba (rei de icono de pestaña de navegador)(pomni en el header)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Joyas locas'; // Propiedad que puede ser utilizada en la plantilla
+
+  // Método para mostrar un mensaje en la consola
+  greet() {
+    console.log('¡Bienvenido a la aplicación!');
+  }
+}
+
+
